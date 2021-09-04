8f0f09d84fd5ce999e55ade4f413af96ab2f2c3f
HaoLy <hao.ly@ubitec.com>
2021-09-04 21:44:06 +0700

    replace SHA with version

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t 142122/complex-client:latest -t 142122/complex-client:$SHA -f ./client/Dockerfile ./client
-docker build -t 142122/complex-server:latest -t 142122/complex-server:$SHA -f ./server/Dockerfile ./server
-docker build -t 142122/complex-worker:latest -t 142122/complex-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t 142122/complex-client:latest -t 142122/complex-client:$VERSION -f ./client/Dockerfile ./client
+docker build -t 142122/complex-server:latest -t 142122/complex-server:$VERSION -f ./server/Dockerfile ./server
+docker build -t 142122/complex-worker:latest -t 142122/complex-worker:$VERSION -f ./worker/Dockerfile ./worker
 docker push 142122/complex-client:latest
 docker push 142122/complex-server:latest
 docker push 142122/complex-worker:latest
 
-docker push 142122/complex-client:$SHA
-docker push 142122/complex-server:$SHA
-docker push 142122/complex-worker:$SHA
+docker push 142122/complex-client:$VERSION
+docker push 142122/complex-server:$VERSION
+docker push 142122/complex-worker:$VERSION
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=142122/complex-server:$SHA
-kubectl set image deployments/client-deployment client=142122/complex-client:$SHA
-kubectl set image deployments/worker-deployment worker=142122/complex-worker:$SHA
+kubectl set image deployments/server-deployment server=142122/complex-server:$VERSION
+kubectl set image deployments/client-deployment client=142122/complex-client:$VERSION
+kubectl set image deployments/worker-deployment worker=142122/complex-worker:$VERSION
 
