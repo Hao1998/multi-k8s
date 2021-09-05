1051ccea4ca43987f2ae51ee316fbdf17fc5c45c
HaoLy <hao.ly@ubitec.com>
2021-09-05 10:32:57 +0700

    revert to normal

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 docker build -t 142122/complex-client:latest -t 142122/complex-client:$SHA -f ./client/Dockerfile ./client
 docker build -t 142122/complex-server:latest -t 142122/complex-server:$SHA -f ./server/Dockerfile ./server
 docker build -t 142122/complex-worker:latest -t 142122/complex-worker:$SHA -f ./worker/Dockerfile ./worker
-git add .
 docker push 142122/complex-client:latest
 docker push 142122/complex-server:latest
 docker push 142122/complex-worker:latest
@@ -10,6 +9,7 @@ docker push 142122/complex-client:$SHA
 docker push 142122/complex-server:$SHA
 docker push 142122/complex-worker:$SHA
 
+git add .
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=142122/complex-server:$SHA
 kubectl set image deployments/client-deployment client=142122/complex-client:$SHA
